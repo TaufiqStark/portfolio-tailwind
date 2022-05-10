c1875fabcfe80cb61cbe76f3b49d3c3b5187c2bd
TaufiqStark <taufiguns@gmail.com>
2022-05-11 01:50:04 +0700

    adding darkmode and link preview

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,6 +1,11 @@
+window.onload = function () {
+    navbarFixed()
+}
+
 // Navbar Fixed 
 const navbar = document.querySelector('#navbar')
-window.onscroll = function(){
+window.onscroll = navbarFixed
+function navbarFixed(){
     if (window.pageYOffset > navbar.offsetTop) {
         navbar.classList.add('navbar-fixed')
     }else{
@@ -8,17 +13,41 @@ window.onscroll = function(){
     }
 }
 
-
 // Hamburger
 const hamburger = document.querySelector('#hamburger')
 const navMenu = document.querySelector('#nav-menu')
+const sections = document.querySelectorAll('section')
 hamburger.addEventListener('click', function(){
     hamburger.classList.toggle('hamburger-active')
     navMenu.classList.toggle('hidden')
+    sections.forEach((e)=>e.classList.toggle('blur-sm'))
 })
 window.addEventListener('click', function(e){
     if (!navMenu.classList.contains('hidden') && hamburger.classList.contains('hamburger-active') && !hamburger.contains(e.target) && !navMenu.contains(e.target)) {
         hamburger.classList.toggle('hamburger-active')
         navMenu.classList.toggle('hidden')
+        sections.forEach((e)=>e.classList.toggle('blur-sm'))
     }
 })
+
+// Dark Mode 
+const html = document.documentElement
+const darkmodeToggle = document.querySelector('#darkmodeToggle')
+darkmodeToggle.addEventListener('click', function(){
+    html.classList.toggle('dark')
+    html.classList.contains('dark') ? localStorage.theme = 'dark' : localStorage.theme = 'light'
+})
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    html.classList.add('dark')
+}else{
+    html.classList.remove('dark')
+}
+
+// document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+//     anchor.addEventListener('click', function (e) {
+//         e.preventDefault();
+//         document.querySelector(this.getAttribute('href')).scrollIntoView({
+//             behavior: 'smooth'
+//         });
+//     });
+// });
